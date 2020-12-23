522491b52cade86601ad036291b281671844b8d9
Christopher Kim <chris.kim1990@gmail.com>
2020-12-23 13:39:31 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,117 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
+end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  loop = 0
+  while loop < 9
+    loop += 1
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+
+    if turn.upcase == "X" || turn.upcase == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], #rows
+                   [0, 3, 6], [1, 4, 7], [2, 5, 8],   #columns
+                   [0, 4, 8], [2, 4, 6] ]             #diagonals
+
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+  if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X") ||
+     (board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O")
+     return win_combo
+   end
+ end
+     false
+   end
+
+def full?(board)
+  if board.include?(" ")
+    false
+  else
+    true
+  end
+end
+
+def draw?(board)
+  if ((won?(board) == false) && (full?(board) == true))
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+   if won?(board) || draw?(board) || full?(board)
+     return true
+   end
+     return false
+ end
+
+def winner(board)
+    if won?(board) != false
+      return board[won?(board)[0]]
+    end
+    return nil
+  end
